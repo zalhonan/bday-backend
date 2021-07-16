4fbda9b9a4a4adfa5dcdbf8e75ac1a0f31884f2f
Konstantin Kuzminov <zal1358@gmail.com>
2021-07-16 23:27:39 +0200

    daemon in progress

--- daemon/main.py
+++ daemon/main.py
@@ -2,7 +2,7 @@ from crud import models
 from crud.database import SessionLocal, engine
 from sqlalchemy import MetaData, Table, text
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import OrderedDict
 
 from crud import schemas
@@ -12,6 +12,12 @@ from functools import cached_property
 models.Base.metadata.create_all(bind=engine)
 
 db = SessionLocal()
+
+events_kinds =  {0: "День рождения",
+  1: "Годовщина свадьбы",
+  2: "Ежегодная встреча",
+  3: "День памяти",
+  4: "Годовщина"}
 
 # загрузим все id
 ids_all = db.query(models.Event.id).all()
@@ -24,14 +30,50 @@ for current_id in ids_all:
     events = json.loads(events_query.events)
 
     for event in events:
-        # * вычисляем дату события. для винды делим на 1000 (что даст только дроид?)
         try:
-            startDate = datetime.fromtimestamp(event['startDate'] // 1000)
+            start_date = datetime.fromtimestamp(event['startDate'] // 1000)
         except:
-            startDate = datetime.fromtimestamp(event['startDate'])
+            start_date = datetime.fromtimestamp(event['startDate'])
 
-            # print(startDate)
-            # print(startDate.month, startDate.day)
+        today_date = datetime.today()
+        tomorrow_date = datetime.today() + timedelta(days=1)
+        inthreedays_date = datetime.today() + timedelta(days=3)
+        inweek_date = datetime.today() + timedelta(days=7)
 
+        event_date = ""
+
+        # дата события - сегодня
+        if today_date.month == start_date.month and today_date.day == start_date.day:
+            event_date = "Сегодня"
+
+        # дата события - завтра
+        if tomorrow_date.month == start_date.month and tomorrow_date.day == start_date.day:
+            event_date = "Завтра"
+
+        # дата события - через три дня
+        if inthreedays_date.month == start_date.month and inthreedays_date.day == start_date.day:
+            event_date = "Через 3 дня"
+
+        # дата события - через неделю
+        if inweek_date.month == start_date.month and inweek_date.day == start_date.day:
+            event_date = "Через неделю"
+
+        # запуск пуш уведомления
+        if event_date != "":
+            push_header = f"{events_kinds[event['eventKind']]} {start_date.day}.{start_date.month}"
+            push_body = f"{event_date}"
+
+            if event['yearKnown']:
+                push_body += f", исполняется лет: {today_date.year - start_date.year}"
+
+            print(push_header)
+            print(push_body)
+
+
+
+
+
+# Годовщина свадьбы 17.07
+# завтра, исполняется лет: 30
 
 db.close()
